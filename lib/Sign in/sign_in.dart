ad7e4586a071141a62e9800201e09788a04a1553
rosha <roshanmathewsofficial@gmail.com>
2022-06-22 12:33:50 +0530

    chore: add sign in page

---
+++ lib/Sign in/sign_in.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _SignIn();
+}
+
+class _SignIn  extends State<SignIn>{
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+
+  );
+  }
+}
